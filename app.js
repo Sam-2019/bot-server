71591f8b7efe7aed882f689e8f859da2be4b6631
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2023-01-17 08:47:53 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ import { connectDB } from "./db/index.js";
 import { router as routes } from "./routes/index.js";
 
 const app = express();
-
+connectDB();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
-connectDB();
-
 app.use("/", routes);
 
 export { app };
