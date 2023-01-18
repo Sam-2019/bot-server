e56a3b9db33c4cc8c4d7044618744e58997d42be
Sam-2019 <samtmartey@gmail.com>
2023-01-18 06:29:28 +0000

    added link-preview-js

--- utils/transformer.js
+++ utils/transformer.js
@@ -1,13 +1,13 @@
-import { linkPreviewGenerator } from "./link_preview.js";
+import { getLinkPreview } from "link-preview-js";
 import { types } from "./switches.js";
 
 export const postTransformer = async (url) => {
-  const info = await linkPreviewGenerator(url);
+  const info = await getLinkPreview(url);
 
   if (!info) {
     return "Unsupported website";
   }
 
-  const output = types(info, url);
+  const output = types(info);
   return `saved ${output}`;
 };
