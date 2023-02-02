996fd7cee01b60bf0ed4d631f5fce8ec9f2c1a29
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2023-02-02 11:39:29 +0000

    Update index.js

--- utils/telegraf/index.js
+++ utils/telegraf/index.js
@@ -4,16 +4,14 @@ import { postTransformer } from "../transformer.js";
 export const bot = new Telegraf(TOKEN);
 
 bot.on("text", async (ctx) => {
-  const text = ctx.message.text;
-//   ctx.reply(await postTransformer(text));
-  console.log(text)
-//   try {
-//     ctx.reply(await postTransformer(text));
-//   } catch (error) {
-//     ctx.reply("Unsupported website");
-//   }
+ const text = ctx.message.text;
+ try {
+  const info = await postTransformer(text);
+  ctx.reply(info);
+ } catch (error) {
+  ctx.reply("failed");
+ }
 });
-
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
