b5824537235af8e334a60687e5497ccab95858dc
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2023-02-02 13:12:43 +0000

    Update switches.js

--- utils/switches.js
+++ utils/switches.js
@@ -1,54 +1,55 @@
 import { data } from "./constants.js";
 import {
-  getAll,
-  saveItem,
-  getArt,
-  getMovies,
-  getTwitter,
-  getJobs,
+ getAll,
+ saveItem,
+ getArt,
+ getMovies,
+ getTwitter,
+ getJobs,
 } from "../db/repository/index.js";
 
 export const switch_route = (type) => {
-  let repository;
+ let repository;
 
-  switch (type) {
-    case data.news.name:
-      repository = getNews();
-      break;
+ switch (type) {
+  case data.news.name:
+   repository = getNews();
+   break;
 
-    case data.art.name:
-      repository = getArt();
-      break;
+  case data.art.name:
+   repository = getArt();
+   break;
 
-    case data.movies.name:
-      repository = getMovies();
-      break;
+  case data.movies.name:
+   repository = getMovies();
+   break;
 
-    case data.twitter.name:
-      repository = getTwitter();
-      break;
+  case data.twitter.name:
+   repository = getTwitter();
+   break;
 
-    case data.jobs.name:
-      repository = getJobs();
-      break;
+  case data.jobs.name:
+   repository = getJobs();
+   break;
 
-    default:
-      repository = getAll();
-  }
-  return repository;
+  default:
+   repository = getAll();
+ }
+ return repository;
 };
 
-export const types = (info) => {
-  const data = {
-    title: info.title ? info.title : null,
-    description: info.description ? info.description : null,
-    domain: info.domain ? info.domain : null,
-    imgURL: info.image ? info.image[0] : null,
-    favicon: info.favicon ? info.favicon : null,
-    url: info.url ? info.url : null,
-    siteName: info.siteName ? info.siteName : null,
-  };
+export const types = async (info) => {
+ console.log(info);
+ const data = {
+  title: info.title ? info.title : "",
+  description: info.description ? info.description : "",
+  domain: info.domain ? info.domain : "",
+  imgURL: info.img ? info.img : "",
+  favicon: info.favicon ? info.favicon : "",
+  url: info.url ? info.url : "",
+  siteName: info.sitename ? info.sitename : "",
+ };
 
-  saveItem(data);
-  return "success";
+ await saveItem(data);
+ return "success";
 };
